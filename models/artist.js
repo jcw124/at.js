4bd56775a75e0a01f72e3a0614d67e0dd30bb319
Janell Williams <jcw_124@sbcglobal.net>
2018-06-13 21:05:32 -0700

    model-artist.js

--- models/artist.js
+++ models/artist.js
@@ -1,3 +1,65 @@
-//Import ORM to create  artist functions
-let orm = require("../config/orm.js");
 
+module.exports = function(sequilize, DataType) {
+    let artists = sequelize.define('artists', {
+        groupName: {
+            type: DataTypes.CHAR,
+            allowNull: false,
+            validate: {
+              len: [1]
+            }
+        }, 
+        contactName:  {
+            type: DataTypes.CHAR,
+            allowNull: false,
+            validate: {
+              len: [1]
+            }
+        }, 
+        contactEmail: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              len: [1]
+            }
+        }, 
+        contactPhone:  {
+            type: DataTypes.STRING
+        }, 
+        personalWebsite:    {
+            type: DataTypes.STRING
+        }, 
+        instagramLink:   {
+            type: DataTypes.STRING
+        }, 
+        facebookLink:   {
+            type: DataTypes.STRING
+        }, 
+        spotifyLink:    {
+            type: DataTypes.STRING
+        }, 
+        twitterLink:    {
+            type: DataTypes.STRING
+        }, 
+        soundcloudLink:    {
+            type: DataTypes.STRING
+        }, 
+        youtubeLink:    {
+            type: DataTypes.STRING
+        }, 
+        appleMusicLink:    {
+            type: DataTypes.STRING
+        }, 
+        otherLink1:   {
+            type: DataTypes.STRING
+        }, 
+        otherLink2:    {
+            type: DataTypes.STRING
+        }, 
+        comments:  DataTypes.TEXT,
+        upcomingProjects: DataTypes.TEXT,
+        lastUpdateDate:   {
+            type: DataTypes.DATE
+        }, 
+    });
+    return Artists;
+};
